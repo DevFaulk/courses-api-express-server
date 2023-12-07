b1a259e9b739a53a79e21a889e6e29e41cf9160a
DevFaulk <faulkdev@gmail.com>
2023-12-07 14:08:22 -0500

    fetch course data

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,18 @@
 const courseTableBody = document.getElementById('courseTableBody');
 
 async function fetchCourses() {
-  const responses = await fetch('');
+  const responses = await fetch('http://localhost:8081/api/courses');
+  const data = await responses.json();
+  createTableData(data);
 }
 
-function createTableData() {}
+function createTableData(courses) {
+  for (const course of courses) {
+    let tr = courseTableBody.insertRow();
+    let tc1 = tr.insertCell();
+    tc1.innerText = course
+  }
+}
 
 window.onload = function () {
   courseTableBody.inner;
