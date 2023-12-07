21e10d0aeb3c1b17862971d7d2d31b9a0df8fc95
DevFaulk <faulkdev@gmail.com>
2023-12-07 18:25:47 -0500

    add data to html divs through js

--- scripts/details.js
+++ scripts/details.js
@@ -11,17 +11,38 @@ function appendCourseData(course) {
   let elementsWithId = Array.from(elements).filter((element) => {
     return element.hasAttribute('id');
   });
-  console.log(course)
+  console.log(course);
   for (const element of elementsWithId) {
     let elementSelected = document.getElementById(
-      `#${element.getAttribute('id')}`
+      `${element.getAttribute('id')}`
     );
-    switch (element) {
-      case element.hasAttribute('#mainHeader'):
-        elementSelected.innerText
+    switch (element.getAttribute('id')) {
+      case 'mainHeader':
+        elementSelected.innerText = `${course.dept} ${course.courseNum}`;
         break;
-
-      default:
+      case 'courseName':
+        elementSelected.innerText = `${course.courseName}`;
+        break;
+      case 'courseDept':
+        elementSelected.innerText = `${course.dept}`;
+        break;
+      case 'courseCode':
+        elementSelected.innerText = `${course.courseNum}`;
+        break;
+      case 'courseInstructor':
+        elementSelected.innerText = `${course.courseInstructor}`;
+        break;
+      case 'courseDescription':
+        elementSelected.innerText = `${course.dept} ${course.courseNum} is Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vitae egestas sem. Fusce neque nunc, porta non finibus eu, venenatis et lorem. Phasellus sed pellentesque lacus. Nullam consequat, mauris eget rutrum euismod, ex nunc vestibulum orci, a vestibulum nisi est vel ipsum. Duis ex erat, dapibus non porttitor quis, efficitur a ligula. Sed pulvinar dignissim neque at lobortis. Praesent quam tortor, convallis vel libero quis, egestas pulvinar lacus. Aliquam nisl turpis, interdum non felis luctus, consectetur molestie arcu. Aenean vehicula id nisl in sollicitudin. Vivamus tincidunt tincidunt ipsum vitae pellentesque. Etiam vitae elementum mi. Aenean vehicula cursus viverra. Nunc iaculis. `;
+        break;
+      case 'courseStart':
+        elementSelected.innerText = `${course.startDate}`;
+        break;
+      case 'courseEnd':
+        elementSelected.innerText = `${course.startDate} ${course.numDays}`;
+        break;
+      case 'courseLength':
+        elementSelected.innerText = `${course.numDays}`;
         break;
     }
   }
