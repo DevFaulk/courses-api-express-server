fd7b07b716b6004d0519d77135a5e1acaa14b09f
DevFaulk <faulkdev@gmail.com>
2023-12-07 13:50:47 -0500

    create page content

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+const courseTableBody = document.getElementById('courseTableBody');
+
+async function fetchCourses() {
+  const responses = await fetch('');
+}
+
+function createTableData() {}
+
+window.onload = function () {
+  courseTableBody.inner;
+};
