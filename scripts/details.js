6d699a57fa87402bd1d5d26806920ee690b87c05
DevFaulk <faulkdev@gmail.com>
2023-12-07 19:09:36 -0500

    add date time

--- scripts/details.js
+++ scripts/details.js
@@ -1,5 +1,4 @@
 'use strict';
-
 async function fetchCourse(id) {
   const response = await fetch(`http://localhost:8081/api/courses/${id}`);
   const data = await response.json();
@@ -36,13 +35,18 @@ function appendCourseData(course) {
         elementSelected.innerText = `${course.dept} ${course.courseNum} is Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vitae egestas sem. Fusce neque nunc, porta non finibus eu, venenatis et lorem. Phasellus sed pellentesque lacus. Nullam consequat, mauris eget rutrum euismod, ex nunc vestibulum orci, a vestibulum nisi est vel ipsum. Duis ex erat, dapibus non porttitor quis, efficitur a ligula. Sed pulvinar dignissim neque at lobortis. Praesent quam tortor, convallis vel libero quis, egestas pulvinar lacus. Aliquam nisl turpis, interdum non felis luctus, consectetur molestie arcu. Aenean vehicula id nisl in sollicitudin. Vivamus tincidunt tincidunt ipsum vitae pellentesque. Etiam vitae elementum mi. Aenean vehicula cursus viverra. Nunc iaculis. `;
         break;
       case 'courseStart':
-        elementSelected.innerText = `${course.startDate}`;
+        elementSelected.innerText = course.startDate;
         break;
       case 'courseEnd':
-        elementSelected.innerText = `${course.startDate} ${course.numDays}`;
+        elementSelected.innerText = `${DateTime.fromFormat(
+          `${course.startDate} 2023`,
+          'DD'
+        )
+          .plus({ days: course.numDays })
+          .toLocaleString(DateTime.DATE_FULL)}`;
         break;
       case 'courseLength':
-        elementSelected.innerText = `${course.numDays}`;
+        elementSelected.innerText = `${course.numDays} days`;
         break;
     }
   }
