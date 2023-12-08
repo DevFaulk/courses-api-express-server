769b2a44e4177496d776ac2ba314a6d44b7ce1ae
DevFaulk <faulkdev@gmail.com>
2023-12-07 19:47:05 -0500

    add awsm css styling

--- scripts/details.js
+++ scripts/details.js
@@ -1,4 +1,6 @@
 'use strict';
+var DateTime = luxon.DateTime;
+
 async function fetchCourse(id) {
   const response = await fetch(`http://localhost:8081/api/courses/${id}`);
   const data = await response.json();
@@ -29,7 +31,7 @@ function appendCourseData(course) {
         elementSelected.innerText = `${course.courseNum}`;
         break;
       case 'courseInstructor':
-        elementSelected.innerText = `${course.courseInstructor}`;
+        elementSelected.innerText = `${course.instructor}`;
         break;
       case 'courseDescription':
         elementSelected.innerText = `${course.dept} ${course.courseNum} is Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vitae egestas sem. Fusce neque nunc, porta non finibus eu, venenatis et lorem. Phasellus sed pellentesque lacus. Nullam consequat, mauris eget rutrum euismod, ex nunc vestibulum orci, a vestibulum nisi est vel ipsum. Duis ex erat, dapibus non porttitor quis, efficitur a ligula. Sed pulvinar dignissim neque at lobortis. Praesent quam tortor, convallis vel libero quis, egestas pulvinar lacus. Aliquam nisl turpis, interdum non felis luctus, consectetur molestie arcu. Aenean vehicula id nisl in sollicitudin. Vivamus tincidunt tincidunt ipsum vitae pellentesque. Etiam vitae elementum mi. Aenean vehicula cursus viverra. Nunc iaculis. `;
@@ -40,10 +42,10 @@ function appendCourseData(course) {
       case 'courseEnd':
         elementSelected.innerText = `${DateTime.fromFormat(
           `${course.startDate} 2023`,
-          'DD'
+          'MMM d yyyy'
         )
           .plus({ days: course.numDays })
-          .toLocaleString(DateTime.DATE_FULL)}`;
+          .toLocaleString(DateTime.DATE_MED)}`;
         break;
       case 'courseLength':
         elementSelected.innerText = `${course.numDays} days`;
